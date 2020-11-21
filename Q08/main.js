e447ad28229383b7afad697b54ab561d32baaf7e
aihamjarrah <ayham.mjarr@gmail.com>
2020-11-21 19:55:00 +0200

    Tried to solve Q8

--- Q08/main.js
+++ Q08/main.js
@@ -6,9 +6,40 @@ console.log('Problem Solving Q: 8');
 Write a function that returns the number of unique items in an array.
 */
 
-function uniqueItems() {
+function uniqueItems(array) {
   // YOUR CODE HERE
+  let number = [] 
+  let count = 0 
+  array.forEach((element,index)=>{
+    if(number[0] === element){
+      continue ;
+
+    }
+    number.unshift(element)
+    count += 1
+  })
+  return count
 }
+  // })
+  // uniqueItems([5, 1, 2, 2, 1, 5]) // => 3
+//   const countItems = (array)=>{ [5, 1, 2, 2, 1, 5]
+//     if(!array.length){
+//       return 0
+//     }
+//     if(number[0]===array[0]){
+//       array.shift()
+//       console.log(array)
+//       return countItems(array)
+//     }
+//     number.unshift(array[0])
+//     count+=1
+//     array.shift()
+//     return countItems(array)
+//   }
+//   countItems(array);
+//   return count;
+
+// }
 
 /* 
 Examples:
